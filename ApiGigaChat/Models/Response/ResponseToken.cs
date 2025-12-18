c517efcfa339d5d2722596dad0e1b31069ddba86
DikiYLexa <student-a502@STUD-RM3-A502>
2025-12-18 15:21:01 +0500

    Выполненная работа на оценку 3. самков

--- ApiGigaChat/Models/Response/ResponseToken.cs
+++ ApiGigaChat/Models/Response/ResponseToken.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ApiGigaChat.Models.Response
 {
-    internal class ResponseToken
+    public class ResponseToken
     {
+        public string access_token { get; set; }
+        public string expires_at { get; set; }
     }
 }
--- ApiGigaChat/Program.cs
+++ ApiGigaChat/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using ApiGigaChat.Classes;
 using ApiGigaChat.Models.Response;
+using Newtonsoft.Json;
 
 namespace ApiGigaChat
 {
     public class Program
     {
+
+        static string ClientId = "019b0341-e893-71fe-983a-cf99db3031f1";
+        static string AuthorizationKey = "MDE5YjAzNDEtZTg5My03MWZlLTk4M2EtY2Y5OWRiMzAzMWYxOjYwNzRhYTdhLTcwOTctNDBmMy04ZjVkLTBkZDIwN2YxZGM3Yw==";
+        static List<Models.Request.Message> DialogHistory =
+        new List<Models.Request.Message>()
+        {
+            //new Models.Request.Message
+            //{
+            //    role = "system",
+            //    content = "Ты — Василий Кандинский"
+            //}
+        };
         static async Task Main(string[] args)
         {
             string Token = await GetToken(ClientId, AuthorizationKey);
@@ -18,12 +32,128 @@ namespace ApiGigaChat
                 Console.WriteLine("Не удалось получить токен");
                 return;
             }
+
+
+
+
+
+            while (true)
+            {
+                Console.Write("Сообщение: ");
+                string message = Console.ReadLine();
+
+                DialogHistory.Add(new Models.Request.Message
+                {
+                    role = "user",
+                    content = message
+                });
+
+                ResponseMessage answer = await GetAnswer(Token, DialogHistory);
+
+                string assistantText = answer.choices[0].message.content;
+                Console.WriteLine("Ответ: " + assistantText);
+
+                DialogHistory.Add(new Models.Request.Message
+                {
+                    role = "assistant",
+                    content = assistantText
+                });
+
+                string fileId = ExtractImageId(assistantText);
+
+                if (!string.IsNullOrEmpty(fileId))
+                {
+                    byte[] img = await DownloadImage(Token, fileId);
+                    string path = SaveImage(img);
+                    WallpaperSetter.SetWallpaper(path);
+                    Console.WriteLine("Обои успешно установлены!");
+                }
+                else
+                {
+                    Console.WriteLine("Ответ не содержит изображения.");
+                }
+
+            }
+
+
         }
 
+        public static async Task<ResponseMessage> GetAnswer(string token, List<Models.Request.Message> messages)
+        {
+
+            ResponseMessage responseMessage = null;
+
+            string Url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions";
+
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, SslPolicyErrors) => true;
+
+                using (HttpClient Client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("Authorization", $"Bearer {token}");
+
+                    Models.Request DataRequest = new Models.Request()
+                    {
+                        model = "GigaChat:2.0.28.2",
+                        messages = messages,
+                        function_call = "auto",
+                        temperature = 0.3,
+                        max_tokens = 1500
+                    };
 
 
+                    string JsonContent = JsonConvert.SerializeObject(DataRequest);
+                    Request.Content = new StringContent(JsonContent, Encoding.UTF8, "application/json");
 
+                    HttpResponseMessage Responce = await Client.SendAsync(Request);
 
+                    if (Responce.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Responce.Content.ReadAsStringAsync();
+                        responseMessage = JsonConvert.DeserializeObject<ResponseMessage>(ResponseContent);
+                    }
+                }
+            }
+            return responseMessage;
+
+        }
+
+        public static string ExtractImageId(string content)
+        {
+            var start = content.IndexOf("src=\"") + 5;
+            var end = content.IndexOf("\"", start);
+            return content.Substring(start, end - start);
+        }
+        public static string SaveImage(byte[] data)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "gigachat_wallpaper.jpg");
+            File.WriteAllBytes(filePath, data);
+            return filePath;
+        }
+        public static async Task<byte[]> DownloadImage(string token, string fileId)
+        {
+            string url = $"https://gigachat.devices.sberbank.ru/api/v1/files/{fileId}/content";
+
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true;
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    response.EnsureSuccessStatusCode();
+
+                    return await response.Content.ReadAsByteArrayAsync();
+                }
+            }
+        }
 
 
         public static async Task<string> GetToken(string rqUID, string bearer)
